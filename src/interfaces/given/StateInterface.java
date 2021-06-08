04da699126296ac47e1876042f996c5afb7a72d4
A <60102843+Myhomeisthevoiditself@users.noreply.github.com>
2021-06-09 00:22:55 +0200

    Add files via upload

---
+++ src/interfaces/given/StateInterface.java
@@ -0,0 +1,26 @@
+/*
+ * @author Pieter Collins, Christof Seiler, Katerina Stankova, Nico Roos, Katharina Schueller
+ * @version 0.99.0
+ *
+ * This interface serves as the API for students in phase 1.
+ */
+
+package interfaces.given;
+
+/**
+ * An interface representing the state of a system described by a differential equation.
+ */
+public interface StateInterface {
+    /**
+     * Update a state to a new state computed by: this + step * rate
+     *
+     * @param step   The time-step of the update
+     * @param rate   The average rate-of-change over the time-step. Has dimensions of [state]/[time].
+     // State[] index 0 is respective 
+     // Time[] index 0
+     * @return The new state after the update. Required to have the same class as 'this'.
+     */
+    public StateInterface addMul(double step, RateInterface rate);
+
+    public String toString();
+}
