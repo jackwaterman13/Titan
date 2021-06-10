4544844b39f6fb8830280546eb6d5eddd59e357b
alexander-safi <72268285+alexander-safi@users.noreply.github.com>
2021-06-10 19:34:03 +0200

    Add files via upload

---
+++ src/titan/fileIO/WindDataReader.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class WindDataReader {
+
+    /**
+     * Reads and saves data from the file whose location is stored inside the class.
+     */
+    public void read()
+    {
+
+    }
+
+
+    /**
+     * Stores the file path inside the class
+     *
+     * @param path - the entire file path where our target data file is at.
+     */
+    public void setFilePath(String path)
+    {
+
+    }
+
+
+    /**
+     * Gets the file data stored in a list
+     *
+     * @return The list containing data objects extracted from the file
+     */
+    public List<E> getList()
+    {
+
+    }
+
+
+    /**
+     * Gets the file path that is remembered inside the class
+     *
+     * @return A String containing the entire path to where the data file is located at
+     */
+    public String getFilePath()
+    {
+
+    }
+
+
+
+}
