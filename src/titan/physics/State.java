4e9476f867448d4eef8fb1ec87f55d8aae6049f1
A <60102843+Myhomeisthevoiditself@users.noreply.github.com>
2021-06-11 17:51:50 +0200

    Var added, method updated
    Class updated to keep track at which time the state gets evaluated

--- src/titan/physics/State.java
+++ src/titan/physics/State.java
@@ -9,7 +9,13 @@ import titan.utility.Rate;
 public class State implements StateInterface, RateInterface {
 	private DataInterface[] objects;
 	private State previous;
+	private double period;
 
+	/**
+	 * Constructs a state given a set of data objects.
+	 *
+	 * @param objects - the objects representing the state. Each object should at least contain a position and velocity
+	 */
 	public State(DataInterface[] objects){ this.objects = objects; }
 
 	/**
@@ -33,6 +39,22 @@ public class State implements StateInterface, RateInterface {
 	public State getPrevious(){ return previous; }
 
 	/**
+	 * Sets the period, time interval t at which this state is calculated.
+	 * So if t0 = 0, t1 = 2, t2 = 4 and this state is at t3, then t3 = 3 * 2 = 6
+	 *
+	 * @param t - the time at which state gets evaluated
+	 */
+	public void setPeriod(double t){ this.period = t; }
+
+	/**
+	 * Accesses the time that the state has been evaluated at.
+	 *
+	 * @return Double representing the time this state has been evaluated in the in simulation
+	 */
+	public double getPeriod(){ return period; }
+
+	/**
+	 * Documentation from the interface:
 	 * Update a state to a new state computed by: this + step * rate
 	 *
 	 * @param step   The time-step of the update
@@ -43,16 +65,17 @@ public class State implements StateInterface, RateInterface {
 	 */
 	public StateInterface addMul(double step, RateInterface rate){
 		Rate r  = (Rate) rate;
-		Vector3dInterface[] dxdt = r.getPosRoc();
-		Vector3dInterface[] dvdt = r.getVelRoc();
+		Vector3dInterface[] xRoc = r.getPosRoc();
+		Vector3dInterface[] vRoc = r.getVelRoc();
 		DataInterface[] result = new DataInterface[objects.length];
 		for(int i = 0; i < objects.length; i++){
-			Vector3dInterface xFinal = objects[i].getPosition().addMul(step, dxdt[i]);
-			Vector3dInterface vFinal = objects[i].getVelocity().addMul(step, dvdt[i]);
+			Vector3dInterface xFinal = objects[i].getPosition().addMul(step, xRoc[i]);
+			Vector3dInterface vFinal = objects[i].getVelocity().addMul(step, vRoc[i]);
 			result[i] = objects[i].update(xFinal, vFinal);
 		}
 		State nextState = new State(result);
 		nextState.setPrevious(this);
+		nextState.setPeriod(getPeriod() + step);
 		return nextState;
 	}
 
